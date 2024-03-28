e5f04b1a43da4d1357ef476e3f155e9be0d632a7
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-28 14:22:45 +0500

    done

--- components/CountryFlag.jsx
+++ components/CountryFlag.jsx
@@ -24,13 +24,18 @@ const CountryFlag = () => {
 
   return (
     <>
-      {countryCode && hasFlag(countryCode) && (
+      {/* {countryCode && hasFlag(countryCode) && (
         <img
           className=" h-[30px] w-[30px] md:h-[35px] md:w-[35px] 2xl:h-[50px] 2xl:w-[50px] rounded-xl 2xl:rounded-2xl bg-white overflow-hidden object-cover "
           alt={countryCode}
           src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode}.svg`}
         />
-      )}
+      )} */}
+      <img
+        className=" h-[30px] w-[30px] md:h-[35px] md:w-[35px] 2xl:h-[50px] 2xl:w-[50px] rounded-xl 2xl:rounded-2xl bg-white overflow-hidden object-cover "
+        alt={countryCode}
+        src="/Logos-icons/uk-flag.png"
+      />
     </>
   );
 };
