22feb0ea064b6ad1cb8313987507112db70d63a7
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-30 19:32:25 +0500

    done

--- components/InternetInformation/Time.jsx
+++ components/InternetInformation/Time.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Time = () => {
-  const currentDate = new Date();
-  const hours = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 10000);
+
+    return () => clearInterval(timerID);
+  }, []);
+
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
   const formattedTime = `${hours < 10 ? "0" + hours : hours}:${
     minutes < 10 ? "0" + minutes : minutes
   }`;
--- hooks/useLocationData.js
+++ hooks/useLocationData.js
@@ -7,20 +7,27 @@ const useLocationData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const position = await getCurrentPosition();
-        const { latitude, longitude } = position.coords;
-        const locationResponse = await axios.get(
-          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
-        );
-        const { city, country } = getLocationDetails(
-          locationResponse.data.address
-        );
-        setLocationData({
-          city,
-          country,
-          latitude,
-          longitude,
-        });
+        let cachedData = sessionStorage.getItem("locationData");
+        if (cachedData) {
+          setLocationData(JSON.parse(cachedData));
+        } else {
+          const position = await getCurrentPosition();
+          const { latitude, longitude } = position.coords;
+          const locationResponse = await axios.get(
+            `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+          );
+          const { city, country } = getLocationDetails(
+            locationResponse.data.address
+          );
+          const newData = {
+            city,
+            country,
+            latitude,
+            longitude,
+          };
+          setLocationData(newData);
+          sessionStorage.setItem("locationData", JSON.stringify(newData));
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
